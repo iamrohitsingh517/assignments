9bb526547e074e44477b2fafe47579ae94aeec35
Rohit Singh <152901894+iamrohitsingh517@users.noreply.github.com>
2023-12-06 15:54:22 +0530

    completed times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,18 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+ const startTime= new Date().getTime();
+ let sum=0;
+ for(i=1;i<=n;i++){
+
+    sum=sum+i;
+ }
+
+ const endtime =new Date().getTime();
+ const TimeTaken = (startTime -endtime)/1000;
+
+ console.log("timeTaken"+TimeTaken);
+
+}
+calculateTime(1000000000);
 }
